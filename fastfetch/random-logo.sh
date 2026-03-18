fe0698e40f9ba17138bb31d71750dfcedec5d73e
melissa <melissagh3009@gmail.com>
2026-03-18 09:21:15 -0400

    changed fastfetch logo script, now its the same as zsh

--- fastfetch/random-logo.sh
+++ fastfetch/random-logo.sh
@@ -5,27 +5,35 @@ OUT="$HOME/.config/fastfetch/logo.txt"
 
 file=$(ls "$DIR" | shuf -n 1)
 
+mapfile -t lines < "$DIR/$file"
+
 colors=(
-$'\e[38;2;245;224;220m'  # rosewater
-$'\e[38;2;242;205;205m'  # flamingo
-$'\e[38;2;245;194;231m'  # pink
-$'\e[38;2;203;166;247m'  # mauve
-$'\e[38;2;250;179;135m'  # peach
-$'\e[38;2;249;226;175m'  # yellow
-$'\e[38;2;166;227;161m'  # green
-$'\e[38;2;148;226;213m'  # teal
-$'\e[38;2;137;220;235m'  # sky
-$'\e[38;2;116;199;236m'  # sapphire
-$'\e[38;2;180;190;254m'  # lavender
+"245;194;231"
+"203;166;247"
+"166;227;161"
+"249;226;175"
+"137;180;250"
+"148;226;213"
 )
 
-c1=${colors[$RANDOM % ${#colors[@]}]}
-c2=${colors[$RANDOM % ${#colors[@]}]}
+start=${colors[$RANDOM % ${#colors[@]}]}
+end=${colors[$RANDOM % ${#colors[@]}]}
 
-content=$(cat "$DIR/$file")
+IFS=';' read -r sr sg sb <<< "$start"
+IFS=';' read -r er eg eb <<< "$end"
 
-content=${content//\$1/$c1}
-content=${content//\$2/$c2}
+count=${#lines[@]}
 
-printf "%b\n" "$content" > "$OUT"
-printf "\e[0m" >> "$OUT"
+> "$OUT"
+
+for i in "${!lines[@]}"; do
+
+    t=$(awk "BEGIN {print $i/($count-1)}")
+
+    r=$(awk "BEGIN {printf \"%d\", $sr + ($er-$sr)*$t}")
+    g=$(awk "BEGIN {printf \"%d\", $sg + ($eg-$sg)*$t}")
+    b=$(awk "BEGIN {printf \"%d\", $sb + ($eb-$sb)*$t}")
+
+    printf "\e[38;2;%d;%d;%dm%s\e[0m\n" "$r" "$g" "$b" "${lines[$i]}" >> "$OUT"
+
+done
